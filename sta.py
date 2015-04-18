e0ce63551f7d5d08f050c0cb7abcfbba8540f5ff
Lloyd Russell <llerussell@gmail.com>
2015-04-18 21:56:08 +0100

    Update sta.py

--- sta.py
+++ sta.py
@@ -3,4 +3,38 @@ sta
 Stim/spike triggered average
 @author: llerussell
 """
+import numpy as np
 
+
+def STA(signal, events, n_pre, n_post, baseline=False):
+    """
+    Compute STA
+
+    Parameters
+    ----------
+    signal
+    events
+    n_pre
+    n_post
+    baseline
+
+    Returns
+    -------
+    avg_trace
+    all_traces
+    """
+
+    all_traces = np.zeros([len(events), n_pre + n_post])
+    # avg_trace = np.zeros([n_pre + n_post])
+
+    for idx, event in enumerate(events):
+        all_traces[idx, :] = signal[event-n_pre:event+n_post]
+
+    # n_samples = n_pre+n_post
+    # chunk_size = np.arange(n_samples)
+    # idx = triggers[:, None] + chunk_size[None, :]
+    # all_traces = input_trace[idx]
+
+    avg_trace = np.mean(all_traces, 0)
+
+    return avg_trace, all_traces
